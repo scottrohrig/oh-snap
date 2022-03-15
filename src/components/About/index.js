76a966d4ea51fe61fb9cc1ec92f0394596aacd33
Scott Rohrig <scott.rohrig@gmail.com>
2022-03-15 14:17:03 -0700

    add cover img

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import coverImage from "../../assets/cover/cover-image.jpg"
+import coverImage from "../../assets/cover/cover-image.jpg";
 
 function About() {
   return (
     <section className="my-5">
       <h1 id="about">Who am I?</h1>
-      <img src={coverImage} className='my-2' style={{width: '100%'}} alt="cover image" />
+      <img src={coverImage} className='my-2' style={{ width: '100%' }} alt="who am I" />
       <p>A little primer on my life and work</p>
     </section>
   );
