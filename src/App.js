d946444b881f4963e1cbddab7ff8a42d4841796b
Scott Rohrig <scott.rohrig@gmail.com>
2022-03-22 15:03:42 -0700

    define lifted categories; pass props to Nav; pass props to Gallery;

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import './App.css';
+import React, { useState } from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
+import Gallery from './components/Gallery';
 
 function App() {
+
+  const [ categories ] = useState( [
+    { name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects', },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, nature' },
+  ] );
+
+  const [ currentCategory, setCurrentCategory ] = useState( categories[ 0 ] );
+
   return (
     // React.createElement("div", : props {}, : children [
     // React.createElement("h1", {}, "Ginger").
     // React.createElement("p", {}, "breed: Brittany Spaniel")
     // ]) is equivalent to return (<div><h1>Ginger</h1><p>breed:...</p></div>)
     <div>
-      <Nav />
+      <Nav
+        categories={ categories }
+        setCurrentCategory={ setCurrentCategory }
+        currentCategory={ currentCategory }
+      />
       <main>
+        <Gallery currentCategory={ currentCategory } ></Gallery>
         <About />
       </main>
     </div>
