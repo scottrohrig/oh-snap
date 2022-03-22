3bca6642da43148b25a8988217b599410868a7d2
Scott Rohrig <scott.rohrig@gmail.com>
2022-03-22 14:57:25 -0700

    add Gallery component

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PhotoList from '../PhotoList';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+
+function Gallery( { currentCategory } ) {
+
+  const { name, description } = currentCategory;
+
+  return (
+    <section>
+      <h1>{ capitalizeFirstLetter( name ) }</h1>
+      <p>{ description }</p>
+      <PhotoList category={ name } />
+    </section>
+  );
+}
+
+export default Gallery;
