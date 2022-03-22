6c7ce29b0a427e9adcc3d5bb5e567a3a2910d071
Scott Rohrig <scott.rohrig@gmail.com>
2022-03-22 15:00:18 -0700

    lift state of nav category to App.js; deconstruct props from App.js into categories and setter; set currentCategory and assign it as active on click;

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,31 +1,51 @@
-import React from 'react';
-const Nav = () => {
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
-  const categories = [
-    { name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects', },
-    { name: 'portraits', description: 'Portraits of people in my life' },
-    { name: 'food', description: 'Delicious delicacies' },
-    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, nature' },
-  ];
+const Nav = ( props ) => {
 
-  function categorySelected( category ) {
-    console.log( category );
-  }
+  const {
+    // ask why this is an empty array?
+    categories = [],
+    setCurrentCategory,
+    currentCategory
+  } = props;
+
+  useEffect( () => {
+    document.title = capitalizeFirstLetter( currentCategory.name );
+  }, [ currentCategory ] );
+
 
   return (
-    <header>
+    <header className='flex-row px-1'>
       <h2>
         <a data-testid='link' href="/">
-          <span role='img' aria-label='camera'> 📸</span> Oh Snap!
+          <span role='img' aria-label='camera'>📸</span> Oh Snap!
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
-          <li className="mx-2"><a data-testid='about' href="#about" onClick={ () => { categorySelected( 'about' ); } }>about me</a></li>
-          <li><span onClick={ () => { categorySelected( 'contact' ); } }>contact</span></li>
+          <li className="mx-2">
+            <a
+              data-testid='about'
+              href="#about"
+              onClick={ () => { 'about'; } }>
+              about me
+            </a>
+          </li>
+          <li><span onClick={ () => { 'contact'; } } >contact</span></li>
           { categories.map( ( category ) => (
-            <li className="mx-1" key={ category.name } >
-              <span onClick={ () => { categorySelected( category.name ); } }>{ category.name }</span>
+            <li
+              className={ `mx-1 ${ currentCategory.name === category.name && 'navActive' }` }
+              key={ category.name } >
+              <span
+                onClick={ () => {
+                  // when the nav link is clicked the currentCategory is set
+                  // to category object matching the name of the clicked category
+                  setCurrentCategory( category );
+                } }
+              >
+                { capitalizeFirstLetter( category.name ) }
+              </span>
             </li>
           ) ) }
         </ul>
